c0f8af27593132fc7e6d086ae1d30855ee37ddcc
MarcelMatko <matko518@gmail.com>
2023-05-29 12:11:27 +0200

    Updated user class

--- eUprava/Data/User.cs
+++ eUprava/Data/User.cs
@@ -3,7 +3,12 @@ namespace eUprava.Data
     public class User
     {
         public int Id { get; set; }
-        public string Username { get; set; }
+        public string Name { get; set; }
+        public string Surname  { get; set; }
+        public string EMSO { get; set; }
+        public string City { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
         public string Password { get; set; }
     }
 }
