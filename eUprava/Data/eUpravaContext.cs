68108604745c6b1f58ee4de8a9cde6e8a87e53cf
Tim Povodnik <47430888+povodniktim@users.noreply.github.com>
2023-05-29 12:04:50 +0200

    Add project files.

---
+++ eUprava/Data/eUpravaContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace eUprava.Data
+{
+    public class eUpravaContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string dbPath = Path.Combine(Directory.GetCurrentDirectory(), "eUprava.db");
+            optionsBuilder.UseSqlite($"Data Source={dbPath}");
+        }
+    }
+}
---
+++ eUprava/Migrations/20230529094321_dbName_change.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace eUprava.Migrations
+{
+    /// <inheritdoc />
+    public partial class dbName_change : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
