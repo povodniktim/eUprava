e073e17c93b7a146c30984982f8fcca20ca359d7
Tim Povodnik <47430888+povodniktim@users.noreply.github.com>
2023-06-03 18:14:40 +0200

    Vloge page functionality added

--- eUprava/Data/Vloga.cs
+++ eUprava/Data/Vloga.cs
@@ -1,13 +1,20 @@
 
+using System.ComponentModel.DataAnnotations;
+
 namespace eUprava.Data
 {
   public class Vloga {
     public int Id { get; set; }
     public User User { get; set; }
+
+    [Required]
     public string NacinIzvajanja { get; set; }
     public DateTime Termin { get; set; }
+    [Required]
     public string ZdravniskoPotrdilo { get; set; }
+    [Required]
     public string EvidencniKarton { get; set; }
+    [Required]
     public string KarticaPrvePomoci { get; set; }
   }
 }
