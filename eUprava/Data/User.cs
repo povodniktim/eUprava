b0f47ae0e79332df5e99f04a3d477d54e94954f9
MarcelMatko <matko518@gmail.com>
2023-06-03 17:23:12 +0200

    OddaneVloge page

--- eUprava/Data/User.cs
+++ eUprava/Data/User.cs
@@ -1,15 +1,16 @@
 namespace eUprava.Data
 {
-    public class User
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Surname  { get; set; }
-        public string EMSO { get; set; }
-        public string City { get; set; }
-        public string Address { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
-        public bool isAdmin { get; set; } = false;
-    }
+  public class User
+  {
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public string EMSO { get; set; }
+    public string City { get; set; }
+    public string Address { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public bool isAdmin { get; set; } = false;
+    public List<Vloga> OddaneVloge { get; set; }
+  }
 }
