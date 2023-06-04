4d5018024b8f7fe4135d0f1f9b5e585b4c2745c0
Tim Povodnik <47430888+povodniktim@users.noreply.github.com>
2023-06-04 21:15:37 +0200

    Db-Update

--- eUprava/Migrations/20230604191521_komentar.Designer.cs
+++ eUprava/Migrations/20230604191521_komentar.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using eUprava.Data;
 
@@ -10,9 +11,11 @@ using eUprava.Data;
 namespace eUprava.Migrations
 {
     [DbContext(typeof(eUpravaContext))]
-    partial class eUpravaContextModelSnapshot : ModelSnapshot
+    [Migration("20230604191521_komentar")]
+    partial class komentar
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.5");
---
+++ eUprava/Migrations/20230604191521_komentar.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace eUprava.Migrations
+{
+    /// <inheritdoc />
+    public partial class komentar : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Komentar",
+                table: "Vloge",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Komentar",
+                table: "Vloge");
+        }
+    }
+}
