49eacc94b59535ff650950b5f03df1aa23e6bbda
Tim Povodnik <47430888+povodniktim@users.noreply.github.com>
2023-06-05 12:39:44 +0200

    KPI's design

---
+++ eUprava/wwwroot/js/custom.js
@@ -0,0 +1,7 @@
+﻿window.changeIcon = function (index, iconClass) {
+    var iconElement = document.querySelectorAll('.circle-icon .icon')[index];
+    if (iconElement) {
+        iconElement.classList.remove('fa fa-hand-pointer-o', 'fa-times');
+        iconElement.classList.add('fa-' + iconClass);
+    }
+};
